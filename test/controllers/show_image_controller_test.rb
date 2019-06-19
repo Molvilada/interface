5f1e26dbbfc8f79486290824860dca023c05cdf4
Laura Daniela Molina Villar <ldmolinav@unal.edu.co>
2019-06-18 21:49:44 -0500

    Add files via upload

---
+++ test/controllers/show_image_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class ShowImageControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
---
+++ test/controllers/wsimages_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class WsimagesControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
